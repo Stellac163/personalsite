9886c338c6a18c99316f8aab3e410e80048313a3
stella <stellac163@163.com>
2026-09-02 23:33:24 +0800

    更新内容 2026/9/2 23:33:24

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,18 +1,23 @@
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useLocation } from "react-router-dom";
 import Background from "./components/Background";
 import Welcome from "./pages/Welcome";
 import Home from "./pages/Home";
 import PostDetail from "./pages/PostDetail";
 
 export default function App() {
+  const location = useLocation();
+  // 主页垫在欢迎页下面；在「/」时欢迎页覆盖其上，进入时上滑露出主页。
+  const showWelcome = location.pathname === "/";
+
   return (
     <>
       <Background />
       <Routes>
-        <Route path="/" element={<Welcome />} />
+        <Route path="/" element={<Home />} />
         <Route path="/home" element={<Home />} />
         <Route path="/post/:id" element={<PostDetail />} />
       </Routes>
+      {showWelcome && <Welcome />}
     </>
   );
 }
